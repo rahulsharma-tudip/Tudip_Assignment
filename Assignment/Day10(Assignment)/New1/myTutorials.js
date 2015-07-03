fcf7036f9d13917c3471e1f89985ff12159bce78
Rahul Sharma <rahul.sharma@tudip.nl>
2015-07-03 19:16:22 +0530

    Tudip Assinment

---
+++ Assignment/Day10(Assignment)/New1/myTutorials.js
@@ -0,0 +1,32 @@
+myFunction([
+{
+"rollno": "1",
+"fname": "Rahul",
+"lname": "Sharma",
+"subjects": "Java"
+},
+{
+"rollno": "2",
+"fname": "Rohit",
+"lname": "Sharma",
+"subjects": "Anatomy"
+},
+{
+"rollno": "3",
+"fname": "Ravi",
+"lname": "Sharma",
+"subjects": "Math"
+},
+{
+"rollno": "4",
+"fname": "RajKumar",
+"lname": "Sharma",
+"subjects": "Physics"
+},
+{
+"rollno": "5",
+"fname": "Yogesh",
+"lname": "Sharma",
+"subjects": "Science"
+}
+])
